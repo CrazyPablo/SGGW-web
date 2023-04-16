4aa9c19d8c20d348f61eb41d0e274aa0fbc400a0
CrazyPablo <71659677+CrazyPablo@users.noreply.github.com>
2023-04-16 23:31:38 +0200

    img

--- lab3/script.js
+++ lab3/script.js
@@ -18,3 +18,40 @@ function resize()
 		element.style.height=max_height+"px";
 	});
 }
+
+function imgshow()
+{
+	document.querySelector("#img_frame").setAttribute("style", "display:block");
+	document.querySelector("#grey").setAttribute("style", "display:block");
+	imgresize();
+}
+
+function imgresize()
+{
+	var img = document.querySelector("#img_frame").querySelector("img");
+	var width = img.naturalWidth;
+	if(window.innerWidth>width)
+	{
+		width=(window.innerWidth-width)/2;
+	}
+	else
+	{
+		width=0;
+	}
+	var height = img.naturalHeight;
+	if(window.innerHeight>height)
+	{
+		height=(window.innerHeight-height)/2;
+	}
+	else
+	{
+		height=0;
+	}
+	img.setAttribute("style",`margin:${height}px ${width}px ${height}px ${width}px`);
+}
+
+function imgclose()
+{
+	document.querySelector("#img_frame").setAttribute("style", "display:none");
+	document.querySelector("#grey").setAttribute("style", "display:none");
+}
